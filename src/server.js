73481fd85213186fd4c0dab508b126b39734acf9
thiago <thiagotsdelima@gmail.com>
2024-01-29 13:55:07 -0300

    created cors/axios

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require("express-async-errors");
 const migrationsRun = require("./database/sqlite/migrations");
 const AppError = require("./utils/AppError");
+const cors = require("cors");
 const express = require('express');
 const routes = require('./routes');
 const uploadConfig = require("./configs/upload");
@@ -14,7 +15,7 @@ app.use(express.json());
 
 
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
-
+app.use(cors());
 app.use(routes);
 
 
