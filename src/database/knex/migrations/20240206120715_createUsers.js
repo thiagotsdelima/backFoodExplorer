64cc7c5dad4895bd050d9a4337ff49ced2c25dce
thiago <thiagotsdelima@gmail.com>
2024-02-06 16:09:17 -0300

    created Role

---
+++ src/database/knex/migrations/20240206120715_createUsers.js
@@ -0,0 +1,14 @@
+exports.up = knex => knex.schema.createTable("users", table =>{
+  table.increments("id");
+  table.text("name");
+  table.text("email");
+  table.text("password");
+
+  table.enum("role", ["admin", "customer"], { useNative: true, enumName:"roles"}).notNullable().default("customer");
+
+  table.text("avatar").defaultTo(null);
+  table.timestamp("created_at").defaultTo(knex.raw("(strftime('%d/%m/%Y %H:%M:%S', 'now', 'localtime'))"));
+  table.timestamp("updated_at").defaultTo(knex.raw("(strftime('%d/%m/%Y %H:%M:%S', 'now', 'localtime'))"));
+})
+
+exports.down = knex => knex.schema.dropTable("users")
