4869ff39393912497376ddbe0ec055137cd64c1a
thiago <thiagotsdelima@gmail.com>
2024-03-11 16:44:03 -0300

    update

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 require("express-async-errors");
-
+require("dotenv/config"); 
 const AppError = require("./utils/AppError");
 const cors = require("cors");
 const express = require('express');
@@ -37,7 +37,7 @@ app.use((error, request, response, next) => {
   });
 });
 
-const PORT = 3333;
+const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => console.log(`
 Server is running on Port ${PORT}
 `));
